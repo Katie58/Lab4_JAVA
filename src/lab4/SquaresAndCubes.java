1340cb96713c89ffdf0ead82b9f83a1dcc1b03ff
katie <kateann58@gmail.com>
2019-01-16 22:38:38 -0500

    fixed bugs

--- src/lab4/SquaresAndCubes.java
+++ src/lab4/SquaresAndCubes.java
@@ -16,7 +16,7 @@ public class SquaresAndCubes {
 			
 			retry = retry();
 		}
-
+		exit();
 		scnr.close();
 	}
 	
@@ -29,14 +29,64 @@ public class SquaresAndCubes {
 		
 		while (!valid) {
 			System.out.print("Enter an integer: ");
-			if (scnr.hasNextInt()) {
-				userInputInt = scnr.nextInt();		
-				valid = true;
-			} else {
-				System.out.println("ENTRY INVALID");
+			String userInputString = scnr.nextLine();
+			if (validate(userInputString)) {
+				valid = true;		
 			}
-			scnr.nextLine();
 		}
+		System.out.println();
+	}
+	
+	public static boolean validate(String input) {
+		String maxInt = Integer.toString(Integer.MAX_VALUE);
+		
+		if (input.isEmpty()) {
+			System.out.println("Maybe check your numlock and try again...");
+			return false;
+		}
+		
+		if (input.charAt(0) == '-') {
+			System.out.println("Try to stay POSITIVE and try again...");
+			return false;
+		}
+		
+		for (int i = 0; i < input.length(); i++) {
+			int x = input.charAt(i);
+			if (!(x >= 48 && x <= 57)) {
+				System.out.println("An integer is a number, try again...");
+				return false;
+			}
+		}
+		
+		if (input.length() > String.valueOf(Integer.MAX_VALUE).length()) {
+			System.out.println("The max value of an integer = " + Integer.MAX_VALUE + ", try again...");
+			return false;
+		} else {
+			if (input.length() == String.valueOf(Integer.MAX_VALUE).length()) {
+				for (int i = 0; i < input.length(); i++) {
+					if (input.charAt(i) < maxInt.charAt(i)) {
+						break;
+					} else if (input.charAt(i) > maxInt.charAt(i)) {
+						System.out.println("Close! However, the max value of an integer = " + Integer.MAX_VALUE + ", try again...");
+						return false;
+					}
+				}
+			}	
+		}
+		
+		userInputInt = Integer.parseInt(input);
+		
+		if (userInputInt == 0) {
+			System.out.println("Everything is equal to 0 which is too boring to display, try again...");
+			return false;
+		}
+		
+		if (userInputInt == (cubed(userInputInt) / userInputInt / userInputInt)) {
+			return true;
+		} else {
+			System.out.println("Yikes! That number is far too large to calculate, try again...");
+			return false;
+		}		
 	}
 	
 	public static int squared(int number) {
@@ -50,23 +100,29 @@ public class SquaresAndCubes {
 	public static void print() {
 		System.out.printf("%-15s %-15s %-15s %n", "Number", "Squared", "Cubed");
 		System.out.printf("%-15s %-15s %-15s %n", repeat('=', 7), repeat('=', 7), repeat('=', 7));
-		for (int i = 0; i < userInputInt; i++) {
+		for (int i = 0; i <= userInputInt; i++) {
 			System.out.printf("%-15s %-15s %-15s %n", i, squared(i), cubed(i));
 		}
 	}
 	
 	public static void multiplicationTable() {
+		int padding = 0;
 		System.out.println("\nMultiplication Table:");
 		for (int i = 1; i <= userInputInt; i++) {
-			System.out.printf("%-5s", i);
+			//System.out.printf("%-11s", i);
+			System.out.print(i + repeat(' ', ((String.valueOf(i * userInputInt).length() + 1) - String.valueOf(i).length())));
+			padding += (String.valueOf(i * userInputInt).length() + 1);
 		}
 		System.out.println();
-		System.out.println(repeat('=', 5 * userInputInt));
+		//System.out.println(repeat('=', 11 * userInputInt));
+		System.out.println(repeat('=', padding - 1));
 		for (int i = 1; i <= userInputInt; i++) {
 			for (int j = 1; j <= userInputInt; j++) {
-				System.out.printf("%-5s", i * j);
+				//System.out.printf("%-11s", i * j);
+				System.out.print((i * j) + repeat(' ', ((String.valueOf(j * userInputInt).length() + 1) - String.valueOf(i * j).length())));
 			}
 			System.out.println();
+			System.out.println(repeat('-', padding - 1));
 		}
 		System.out.println();
 	}
@@ -95,5 +151,9 @@ public class SquaresAndCubes {
 		else {
 			return false;
 		}
+	}
+	
+	public static void exit() {
+		System.out.println("GOODBYE!");
 	}
 }
